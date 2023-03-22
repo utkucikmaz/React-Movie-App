56b05e48c5526a640b1ea193f95461f02a47317d
utkucikmaz <utkucikmaz@gmail.com>
2023-03-22 01:23:20 +0100

    login page

--- my-app/src/pages/Login.jsx
+++ my-app/src/pages/Login.jsx
@@ -1,7 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Login() {
+
+  const navigate = useNavigate();
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(email && password){
+      navigate('/')
+    }
+    else{
+      document.getElementById("enter-password").hidden = false; 
+    }
+  }
+
   return (
-    <div>Login</div>
+    <div className='login'>
+      <div>
+        <img src='https://picsum.photos/800/850?grayscale' alt='random photos'/>
+      </div>
+      <div className='login-form'>
+        <h1 className='form-title display-3'>Login</h1>
+        <form id='login' onSubmit={handleSubmit}>
+          <div className='mb-5'>
+            <label htmlFor='email' className='form-label display-4'>
+              Email
+            </label>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              placeholder='Enter your Email adress..'
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+          </div>
+          <div className='mb-5'>
+            <label htmlFor='password' className='form-label display-4'>
+              Password
+            </label>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              placeholder='Enter your Password...'
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+          </div>
+          <input
+            type='submit'
+            className='btn btn-outline-dark form-control'
+            value='Login'
+
+          />  
+          <p hidden id='enter-password'>Please fill the form and then click Login again</p>
+        </form>
+      </div>
+    </div>
   )
 }
