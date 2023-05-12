6aae2e9a462ba39a18a3cb63c47def1b2da37884
utkucikmaz <utkucikmaz@gmail.com>
2023-05-13 01:40:21 +0200

    better reducer

--- my-app/src/hooks/use-movies.ts
+++ my-app/src/hooks/use-movies.ts
@@ -2,55 +2,71 @@ import { useEffect, useReducer } from "react";
 import { getMovies, MovieResult } from "../api/get-movies.api";
 
 type State = {
-  movies: MovieResult;
-  notFound: boolean;
-  loading: boolean;
+    movies: MovieResult;
+    notFound: boolean;
+    loading: boolean;
 };
 
 type Action =
-  | { type: "setMovies"; payload: MovieResult }
-  | { type: "setNotFound"; payload: boolean }
-  | { type: "setLoading"; payload: boolean };
+    | { type: "setMovies"; payload: MovieResult }
+    | { type: "setNotFound"; payload: boolean }
+    | { type: "setLoading"; payload: boolean }
+    | { type: "moviesFetched"; payload: MovieResult }
+    | { type: "moviesError"; payload: boolean };
 
 const initialState: State = {
-  movies: [],
-  notFound: false,
-  loading: false,
+    movies: [],
+    notFound: false,
+    loading: false,
 };
 
 const reducer = (state: State, action: Action): State => {
-  switch (action.type) {
-    case "setMovies":
-      return { ...state, movies: action.payload };
-    case "setNotFound":
-      return { ...state, notFound: action.payload };
-    case "setLoading":
-      return { ...state, loading: action.payload };
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case "setMovies":
+            return { ...state, movies: action.payload };
+        case "setNotFound":
+            return { ...state, notFound: action.payload };
+        case "setLoading":
+            return { ...state, loading: action.payload };
+        case "moviesFetched":
+            return {
+                ...state,
+                movies: action.payload,
+                notFound: false,
+                loading: false,
+            };
+        case "moviesError":
+            return {
+                ...state,
+                movies: [],
+                notFound: action.payload,
+            };
+        default:
+            return state;
+    }
 };
 
 export const useMovies = (searchTerm: string) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+    const [state, dispatch] = useReducer(reducer, initialState);
 
-  useEffect(() => {
-    const fetchMovies = async () => {
-      try {
-        dispatch({ type: "setLoading", payload: true });
-        const data = await getMovies(searchTerm);
-        dispatch({ type: "setMovies", payload: data.results });
-        dispatch({ type: "setNotFound", payload: false });
-      } catch (error: any) {
-        dispatch({ type: "setMovies", payload: [] });
-        dispatch({ type: "setNotFound", payload: error.status === 404 });
-      } finally {
-        dispatch({ type: "setLoading", payload: false });
-      }
-    };
+    useEffect(() => {
+        const fetchMovies = async () => {
+            try {
+                dispatch({ type: "setLoading", payload: true });
+                const data = await getMovies(searchTerm);
+                dispatch({ type: "moviesFetched", payload: data.results });
+            } catch (error: any) {
+                dispatch({
+                    type: "moviesError",
+                    payload: error.status === 404,
+                });
+            } finally {
+                dispatch({ type: "setLoading", payload: false });
+            }
+        };
 
-    fetchMovies();
-  }, [searchTerm, dispatch]);
+        fetchMovies();
+    }, [searchTerm, dispatch]);
 
-  return [state.movies, state.loading, state.notFound] as const;
+    return [state.movies, state.loading, state.notFound] as const;
 };
